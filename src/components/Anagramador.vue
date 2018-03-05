a695cba635d2c025af78e12fee1bcb10393e349b
Jimmy Porto <ireturnerd@gmail.com>
2018-03-05 18:21:22 -0300

    Updated components

---
+++ src/components/Anagramador.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="conteudo">
+    <div class="botoes">
+        <button v-for="letra in letras">{{ letra }}</button>
+    </div>
+    <div class="palavras">
+        <ul>
+            <li v-for="palavra in palavras">{{ palavra }}</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      letras: [],
+      palavras: []
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
